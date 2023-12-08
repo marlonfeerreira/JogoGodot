2080801a2df198b95d3a88604cee940b756c794c
marlonsilvape@gmail.com <marlonsilvape@gmail.com>
2023-12-08 10:01:23 -0300

    first commit

---
+++ Scripts/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var grav = 10
+var speed = 300
+
+func _process(delta):
+	
+	if !is_on_floor():
+		velocity.y += grav
+		
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+		$"Duck(16x16)".flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+		$"Duck(16x16)".flip_h = true
+		
+	else:
+		velocity.x = 0 
+	
+	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
+		velocity.y -= 300
+		
+	move_and_slide()
+	
+		
